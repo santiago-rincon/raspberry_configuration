e78e25d3c4dbc395aeeeb876f0414339389da810
Santiago Rincon <cristiansantiago260501@gmail.com>
2023-10-05 15:23:21 -0500

    Script enable ESP32 finish

--- services/enable_esp.py
+++ services/enable_esp.py
@@ -1,3 +1,7 @@
-import os
-
-os.mkdir("new_folder")
+import serial, os
+ser = serial.Serial('/dev/ttyAMA1', 9600, timeout=1)
+ser.reset_input_buffer()
+smd = "a\n"
+smds = smd.encode('utf-8')
+ser.write(smds)
+os.system('echo $(date) >> $HOME/Desktop/Proyecto/logs/pulses.log')
